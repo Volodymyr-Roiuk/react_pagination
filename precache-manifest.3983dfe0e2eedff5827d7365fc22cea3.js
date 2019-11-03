e1309264e2f28b68c129c42535accf981ced2048
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-03 15:04:45 +0300

    Updates

---
+++ precache-manifest.3983dfe0e2eedff5827d7365fc22cea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5de65652ca37e26e273c01585400943",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "972565cbb10639b4da36",
+    "url": "/react_pagination/static/css/main.b97d3cd5.chunk.css"
+  },
+  {
+    "revision": "97dbd3a18463fa12649f",
+    "url": "/react_pagination/static/js/2.89cb6c27.chunk.js"
+  },
+  {
+    "revision": "972565cbb10639b4da36",
+    "url": "/react_pagination/static/js/main.685bbdc7.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
